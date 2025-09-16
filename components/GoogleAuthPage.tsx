79266f2f9fb944b628474a73aa36b0d424673e2e
Christian Wang <christianwang39@gmail.com>
2025-09-16 00:40:54 -0400

    small design changes

--- components/GoogleAuthPage.tsx
+++ components/GoogleAuthPage.tsx
@@ -21,7 +21,7 @@ export default function GoogleAuthPage() {
     <div className="fixed inset-0 bg-black text-white overflow-hidden flex items-center justify-center">
       {/* Logo in bottom-left (same position/size as the small circle) */}
       <div className="absolute bottom-6 left-6 z-50 pointer-events-none select-none">
-        <Image src="/blackLogo.svg" alt="BAFC Logo" width={24} height={24} className="w-6 h-6" priority />
+        <Image src="/blackLogo.svg" alt="BAFC Logo" width={32} height={32} className="w-10 h-10" priority />
       </div>
 
       <div className="flex w-full h-full">
@@ -74,7 +74,7 @@ export default function GoogleAuthPage() {
                     d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
                   />
                 </svg>
-                <span className="ml-2">{isLoading ? 'Signing in…' : 'Sign in with Google'}</span>
+                <span className="ml-2">{isLoading ? 'Signing in…' : 'Google'}</span>
               </button>
 
               {/* Terms/help text removed as requested */}
